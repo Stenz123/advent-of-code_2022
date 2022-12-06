f48205d7c9ab6e1861256f463f165629eb3bbaae
Michael Stenz <michael.stenz2006@gmail.com>
2022-12-06 14:52:00 +0100

    initial commit

--- src/aoc/day/days/Day1.java
+++ src/aoc/day/days/Day1.java
@@ -1,53 +1,15 @@
 package aoc.day.days;
 
-import aoc.day.*;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+import aoc.day.Day;
 
 public class Day1 extends Day {
-    public Day1(){
-        super();
-    }
-
-    @Override
-    public Solution solve() {
-        List<String> rawInput = this.getDayInput();
-
-        List<List<Integer>> elves = new ArrayList<>();
-
-        List<Integer> items = new ArrayList<>();
-        for (int i = 0; i < rawInput.size(); i++) {
-            if (rawInput.get(i).equals("")) {
-                elves.add(items);
-                items = new ArrayList<>();
-            } else {
-                items.add(Integer.parseInt(rawInput.get(i)));
-            }
-        }
-
-        List<Integer> sums = new ArrayList<>();
-
-        for (int i = 0; i < elves.size(); i++) {
-            int sum = 0;
-            for (int item : elves.get(i)) {
-                sum += item;
-            }
-            sums.add(sum);
-        }
-        Collections.sort(sums, Collections.reverseOrder());
-
-        return new Solution(this.getDay(),sums.get(0),sums.get(0)+sums.get(1)+sums.get(2));
-    }
-
     @Override
     public Object part1() {
-        return null;
+        return -1;
     }
 
     @Override
     public Object part2() {
-        return null;
+        return -1;
     }
 }
